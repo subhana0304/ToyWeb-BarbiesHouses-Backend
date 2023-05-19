320989e6bca67370bc2ac0dcb40d6ae5de305d27
Subhana <subhana5721194@gmail.com>
2023-05-19 21:43:23 +0600

    Get data from single id for see details page

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -41,11 +41,18 @@ async function run() {
       const cursor = addBarbieCollection.find();
       const result = await cursor.toArray();
       res.send(result);
+    });
+
+    app.get('/barbies/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await addBarbieCollection.findOne(query)
+      res.send(result);
     })
 
     app.post('/barbies', async(req, res)=>{
       const addBarbie = req.body;
-      console.log(addBarbie);
+      // console.log(addBarbie);
       const result = await addBarbieCollection.insertOne(addBarbie)
       res.send(result);
     })
